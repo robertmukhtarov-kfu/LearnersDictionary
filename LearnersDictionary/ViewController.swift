2d1f503d1e2d152697a427c6110a6074592ae576
Robert <rrmukhtarov@stud.kpfu.ru>
2021-03-01 15:44:07 +0300

    Initial Commit

---
+++ LearnersDictionary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LearnersDictionary
+//
+//  Created by Robert Mukhtarov on 01.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
